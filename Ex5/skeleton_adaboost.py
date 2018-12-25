60ba37af3825c9fb833dbe824221f3ecf9008a49
gefen keinan <gefenk9@gmail.com>
2018-12-25 18:05:59 +0200

    working version with fast pick of weak learnable

--- Ex5/skeleton_adaboost.py
+++ Ex5/skeleton_adaboost.py
@@ -1,12 +1,11 @@
 #################################
-# Your name:
+# Your name:gefen keinan
 #################################
 
 # Please import and use stuff only from the packages numpy, sklearn, matplotlib.
 from matplotlib import pyplot as plt
 import numpy as np
 from process_data import parse_data
-
 np.random.seed(7)
 
 
@@ -23,88 +22,155 @@ def run_adaboost(X_train, y_train, T):
             A list of T float values, which are the alpha values obtained in every 
             iteration of the algorithm.
     """
-    # TODO: add your code here
-    hypothesis = _create_hypothesis(X_train)
-    D =[]
-    m = len(X_train)
-    D_0 = [1/m]*m #First disrebution
-    D.append(D_0)
-    for i in range(T):
-        h_t, epsilon = _pick_best_learner(D[i], X_train, y_train)
-        a_t = 0.5*np.log((1-epsilon)/epsilon)
-        D_next = []
+    n = len(X_train)
+    alphas = []
+    hypothesises = []
 
-        normalized_factor = 0
-        for z in range(m):
-            normalized_factor += D[i][z] * np.exp(-a_t * y_train[z] * h_t(X_train[z]))
-        for j in range(m):
-            d_next_j = (D[i][j] * np.exp(-a_t*y_train[j]*h_t(X_train[j]))) / normalized_factor
-            D_next.append(d_next_j)
+    # Initialize distribution weights
+    Dt = np.array([1.0 / n for i in range(n)])
+    for t in range(T):
+        # Calculate erm_decision_stump and its error foreach class A and B
+        hypothesis_A, error_A = erm_decision_stump(X_train, y_train, Dt, 1)
+        hypothesis_B, error_B = erm_decision_stump(X_train, y_train, Dt, np.negative(1))
+        if error_A < error_B:
+            hypothesis_t = hypothesis_A
+            error_t = error_A
+        else:
+            hypothesis_t = hypothesis_B
+            error_t = error_B
+
+        alpha_t = 0.5 * np.log((1 - error_t) / error_t)
+
+        hypothesises.append(hypothesis_t)
+        alphas.append(alpha_t)
+
+        # Update distribution
+        softmax_vals = np.array(
+            [np.exp(np.negative(alpha_t * y_train[j] * hypothesis_t[0](X_train[j]))) for j in range(n)])
+        Zt = np.dot(Dt, softmax_vals)
+        new_softmax_vals = softmax_vals * (1.0 / Zt)
+        Dt = np.multiply(Dt, new_softmax_vals)
+
+        curr_boosted_h = lambda x: np.sign(np.sum([(hypothesises[j][0](x) * alphas[j]) for j in range(t + 1)]))
+        print ("Error decreased to:", calculate_error(X_train, y_train, curr_boosted_h))
+
+    return hypothesises, alphas
+
 
 
 ##############################################
 # You can add more methods here, if needed.
 
-def _pick_best_learner():
-    pass
+def erm_decision_stump(X_train, y_train, D, b):
+    m = len(X_train)
+    d = len(X_train[0])
 
-def _create_hypothesis(trainging_x):
-    """
-    from the trainging data we will return all hypothesis from the two classes
-    :return:
-    """
-    hypothesis = []
-    for x in trainging_x:
-        for index, value in enumerate(x):
-            hypothes = Hypothesa_class_A(value, index)
-            hypothesis.append(hypothes)
-            hypothes = Hypothesa_class_B(value, index)
-            hypothesis.append(hypothes)
-    return hypothesis
+    labeled_data = list(zip(X_train, y_train, D))
+    x_index,y_index,D_index = 0,1,2
 
-class Hypothesa_class_A:
-    def __init__(self, theta, feature_index):
-        self.theta = theta
-        self.feature_index = feature_index
+    best_f = float('inf')
+    theta = X_train[0][0] - 1
+    best_j = 0
 
-    def classify(self, point):
-        if point[self.feature_index] > self.theta:
-            return -1
-        else:
-            return 1
+    for j in range(d):
+        sorted_by_j = sorted(labeled_data, key=lambda labeled_point: labeled_point[x_index][j])
 
-class Hypothesa_class_B:
-    def __init__(self, theta, feature_index):
-        self.theta = theta
-        self.feature_index = feature_index
+        aprox_last_labeled_point = [(sorted_by_j[m - 1][x_index][j] + 1) for i in range(d)]
+        sorted_by_j.append((aprox_last_labeled_point, float('inf'), float('inf')))
+        curr_f = np.sum([labeled_point[D_index] for labeled_point in sorted_by_j if labeled_point[y_index] == b])
 
-    def classify(self, point):
-        if point[self.feature_index] > self.theta:
-            return 1
-        else:
-            return -1
+        if curr_f < best_f:
+            best_f = curr_f
+            theta = sorted_by_j[0][x_index][j] - 1
+            best_j = j
+
+        # Sorted points iteration
+        for i in range(m):
+            curr_f = curr_f - (b * (sorted_by_j[i][y_index] * sorted_by_j[i][D_index]))
+
+            if (curr_f < best_f) and (sorted_by_j[i][x_index][j] != sorted_by_j[i + 1][x_index][j]):
+                best_f = curr_f
+                theta = 0.5 * (sorted_by_j[i][x_index][j] + sorted_by_j[i + 1][x_index][j])
+                best_j = j
+
+    return (lambda x: np.sign(theta - x[best_j]) * b, best_j, theta), best_f
+
+def calculate_error(data, labels, hypothesis):
+    error = 0
+    for i in range(len(data)):
+        if hypothesis(data[i]) != labels[i]:
+            error += 1
+    return float(error) / len(data)
 
 
 ##############################################
 
+
+def b(X_train,y_train, vocab):
+    hypotheses, alphas = run_adaboost(X_train, y_train, T=10)
+    for t in range(10):
+        print(vocab[hypotheses[t][1]])
 
 def main():
     data = parse_data()
     if not data:
         return
     (X_train, y_train, X_test, y_test, vocab) = data
+    b(X_train,y_train,vocab)
 
-    hypotheses, alpha_vals = run_adaboost(X_train, y_train, T=80)
-
-    ##############################################
-    # You can add more methods here, if needed.
+    T = 80
+    hypotheses, alpha_vals = run_adaboost(X_train, y_train, T)
 
 
+    train_errors = []
+    test_errors = []
+    train_func_l = []
+    test_func_l  = []
+    for t in range(T):
+        curr_boosted_h = lambda x: np.sign(
+            np.sum([(hypotheses[i][0](x) * alpha_vals[i]) for i in range(t + 1)]))
+
+        train_errors.append(calculate_error(X_train, y_train, curr_boosted_h))
+        test_errors.append(calculate_error(X_test, y_test, curr_boosted_h))
+
+        # For section C
+        L_softmax_vals_func = lambda Xi,Yi:\
+            [np.exp(np.negative(Yi * np.sum([alpha_vals[j] * hypotheses[j][0](Xi) for j in range(t)])))]
+        L_func = lambda X,Y: 1 / len(X) * np.sum([L_softmax_vals_func(X[i], Y[i]) for i in range(len(X))])
+        train_func_l.append(L_func(X_train, y_train))
+        test_func_l.append(L_func(X_test, y_test))
+
+
+
+    # 1(a)
+    plt.figure()
+    plt.title("errors per t")
+    train_error_points, = plt.plot(np.arange(1, T + 1, 1), train_errors,  label='Train Errors')
+    test_error_points, = plt.plot(np.arange(1, T + 1, 1), test_errors,  label='Test Errors')
+    plt.legend(handles=[train_error_points, test_error_points])
+    plt.grid()
+    plt.xlabel('iterations')
+    plt.ylabel('error')
+    plt.axis([0, 85, 0, 0.6])
+    plt.xticks(np.arange(0, 85, 10))
+    plt.yticks(np.arange(0, 0.6, 0.05))
+    plt.savefig('1(a).png')
+
+    #  1(c)
+    plt.figure()
+    plt.title("test train loss , # of WL")
+    train_func_l_points, = plt.plot(np.arange(1, T + 1, 1), train_func_l,  label='train loss')
+    test_func_l_points, = plt.plot(np.arange(1, T + 1, 1), test_func_l,  label='test loss')
+    plt.legend(handles=[train_func_l_points, test_func_l_points])
+    plt.grid()
+    plt.xlabel('t')
+    plt.ylabel('loss')
+    plt.axis([0, 85, 0, 1.1])
+    plt.xticks(np.arange(0, 85, 10))
+    plt.yticks(np.arange(0, 1.1, 0.05))
+    plt.savefig('1(c).png')
 
     ##############################################
 
 if __name__ == '__main__':
     main()
-
-
-
