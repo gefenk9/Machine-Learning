6c66a7d91246292840a36e8ffdd5ad9d467ea8e9
gefen keinan <gefenk9@gmail.com>
2019-01-10 22:50:29 +0200

    pca skeleton

--- Ex6/pca.py
+++ Ex6/pca.py
@@ -1,37 +1,38 @@
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_lfw_people
+import numpy as np
 
-def plot_vector_as_image(image, h, w):
-	"""
-	utility function to plot a vector as image.
-	Args:
-	image - vector of pixels
-	h, w - dimesnions of original pi
-	"""
-	plt.imshow(image.reshape((h, w)), cmap=plt.cm.gray)
-  plt.title(title, size=12)
-  plt.show()
+def plot_vector_as_image(image, h, w, i=0):
+    """
+    utility function to plot a vector as image.
+    Args:
+    image - vector of pixels
+    h, w - dimesnions of original pi
+    """
+    plt.imshow(image.reshape((h, w)), cmap=plt.cm.gray)
+    plt.title("", size=12)
+    plt.savefig(str(i))
 
 def get_pictures_by_name(name='Ariel Sharon'):
-	"""
-	Given a name returns all the pictures of the person with this specific name.
-	YOU CAN CHANGE THIS FUNCTION!
-	THIS IS JUST AN EXAMPLE, FEEL FREE TO CHANGE IT!
-	"""
-	lfw_people = load_data()
-	selected_images = []
-	n_samples, h, w = lfw_people.images.shape
-	target_label = list(lfw_people.target_names).index(name)
-	for image, target in zip(lfw_people.images, lfw_people.target):
-		if (target == target_label):
-			image_vector = image.reshape((h*w, 1))
-			selected_images.append(image_vector)
-	return selected_images, h, w
+    """
+    Given a name returns all the pictures of the person with this specific name.
+    YOU CAN CHANGE THIS FUNCTION!
+    THIS IS JUST AN EXAMPLE, FEEL FREE TO CHANGE IT!
+    """
+    lfw_people = load_data()
+    selected_images = []
+    n_samples, h, w = lfw_people.images.shape
+    target_label = list(lfw_people.target_names).index(name)
+    for image, target in zip(lfw_people.images, lfw_people.target):
+        if (target == target_label):
+            image_vector = image.reshape((h*w, 1))
+            selected_images.append(image_vector)
+    return np.hstack(selected_images).T, h, w
 
 def load_data():
-	# Don't change the resize factor!!!
-	lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-	return lfw_people
+    # Don't change the resize factor!!!
+    lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+    return lfw_people
 
 ######################################################################################
 """
@@ -39,19 +40,28 @@ Other then the PCA function below the rest of the functions are yours to change.
 """
 
 def PCA(X, k):
-	"""
-	Compute PCA on the given matrix.
+    """
+    Compute PCA on the given matrix.
 
-	Args:
-		X - Matrix of dimesions (n,d). Where n is the number of sample points and d is the dimension of each sample.
-		For example, if we have 10 pictures and each picture is a vector of 100 pixels then the dimesion of the matrix would be (10,100).
-		k - number of eigenvectors to return
+    Args:
+        X - Matrix of dimesions (n,d). Where n is the number of sample points and d is the dimension of each sample.
+        For example, if we have 10 pictures and each picture is a vector of 100 pixels then the dimesion of the matrix would be (10,100).
+        k - number of eigenvectors to return
 
-	Returns:
-	  U - Matrix with dimension (k,d). The matrix should be composed out of k eigenvectors corresponding to the largest k eigenvectors
-	  		of the covariance matrix.
-	  S - k largest eigenvalues of the covariance matrix. vector of dimension (k, 1)
-	"""
-	U = None
-	S = None
-	return U, S
+    Returns:
+      U - Matrix with dimension (k,d). The matrix should be composed out of k eigenvectors corresponding to the largest k eigenvectors
+            of the covariance matrix.
+      S - k largest eigenvalues of the covariance matrix. vector of dimension (k, 1)
+    """
+    U = None
+    S = None
+    X = X - X.mean(axis=1, keepdims=True)
+    U, S, vh = np.linalg.svd(np.dot(X.T, X) * (1 / X.shape[0]))
+    return U[:, :k].T, S[:k].reshape((k, 1))
+
+
+lfw = load_data()
+selected_images, h, w = get_pictures_by_name()
+U, S = PCA(selected_images, 10)
+for i in range(len(U)):
+    plot_vector_as_image(U[i], h, w, i)
